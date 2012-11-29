cce6e3ce36e6d527fdf7d5bcfac19c860b38c8bc
cnvandev <chris.vandevelde@uwaterloo.ca>
2012-11-28 20:33:31 -0800

    I need to get better with typos, I commit far too often.

--- sample.py
+++ sample.py
@@ -12,9 +12,9 @@ print html(
     body(
         comment("Version one of my page, thanks for checking out the source."),
         h1({"state": "jolly", "stuff": "awesome", "no-really": "this is fucking rad"}, "This Is My Page"),
-        p("Check it out, this is a page! It's pretty rad, I'm pretty proud of it, to be honest. I hope you like it!", Class="thing"),
+        p({"class": "thing"}, "Check it out, this is a page! It's pretty rad, I'm pretty proud of it, to be honest. I hope you like it!"),
         p("There's not much here, I'm just learning, but here's hoping more will come."),
-        p(""),
+        p(),
         comment("Important remarks go towards the end."),
         p("Chris Vandevelde is super-hot.")
     )
